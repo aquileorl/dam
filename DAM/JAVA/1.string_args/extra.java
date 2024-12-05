6854a222a795ae8c2c60801ed99e75661263abe0
EmilioRL <ejruizlinares@gmail.com>
2024-12-05 09:50:03 +0100

    clase extra profe nuevo

---
+++ DAM/JAVA/1.string_args/extra.java
@@ -0,0 +1,12 @@
+
+import java.util.Scanner;
+
+public class extra{
+    public static void main(String[] args){
+        /*
+        Añadir entrecomillado dentro del imprimir por pantalla
+        usando el carácter especial \" \"
+         */
+        System.out.println("Hola qué tal estás \"don gato\"");
+    }
+}
