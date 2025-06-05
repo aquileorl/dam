4a1b2c9cf829f92b3c2e25b632bc9494ba7c087b
aquileorl <ejruizlinares@gmail.com>
2025-06-05 20:37:04 +0200

    content update

--- DAM/MASTER/Javascript_DOM/app.js
+++ DAM/MASTER/Javascript_DOM/app.js
@@ -12,3 +12,26 @@ console.log(titulo.parentElement);
 
 const divpp = document.querySelector("div");
 console.log(divpp.children);
+
+//Crear elementos
+
+let nuevoH2 = document.createElement("h2");
+let nuevoParrafo = document.createElement("p");
+
+//Crear nodos de texto
+let textoH2 = document.createTextNode("Hola Mundo Cruel");
+let textoParrafo = document.createTextNode("Viva España y Hala Madrid");
+
+let cabecera = document.querySelector("h1");
+
+//Añadimos nodos hijos al elemento seleccionado
+cabecera.appendChild(nuevoH2);
+cabecera.appendChild(nuevoParrafo);
+
+//Recuperamos el nodo padre del elemento seleccionado
+let padre = cabecera.parentNode;
+
+//Insertamos nuevoH2 justo antes del elemento cabecera
+padre.insertBefore(nuevoH2, cabecera);
+
+
