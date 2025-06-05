261533c4de85a943e7c83fa7910683fc82b37fe4
aquileorl <ejruizlinares@gmail.com>
2025-06-05 19:42:49 +0200

    Intro DOM

---
+++ DAM/MASTER/Javascript_DOM/app.js
@@ -0,0 +1,14 @@
+
+const titulo = document.querySelector("h1");
+console.log(titulo);
+
+const divP = document.querySelector("div > p");
+console.log(divP);
+
+const allDivP = document.querySelectorAll("div > p");
+console.log(allDivP);
+
+console.log(titulo.parentElement);
+
+const divpp = document.querySelector("div");
+console.log(divpp.children);
