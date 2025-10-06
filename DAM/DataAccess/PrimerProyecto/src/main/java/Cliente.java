59f368bab2d06064b2b15254d9e5187d4b33d419
aquileorl <ejruizlinares@gmail.com>
2025-10-06 18:46:52 +0200

    Clase Cliente creada

---
+++ DAM/DataAccess/PrimerProyecto/src/main/java/Cliente.java
@@ -0,0 +1,18 @@
+public class Cliente {
+
+    private int id;
+    private string nombre;
+    private string email;
+    private double saldo;
+
+    public Cliente(){
+
+    }
+
+    public Cliente(int id, string nombre, string email, double saldo) {
+        this.id = id;
+        this.nombre = nombre;
+        this.email = email;
+        this.saldo = saldo;
+    }
+}
