f93b85144ed1d598c029deea31b64336da82958f
aquileorl <ejruizlinares@gmail.com>
2025-10-16 18:28:20 +0200

    Creado el metodo para main activity y launcher

--- DAM/Mobile_Development/MyFirstApp/app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ DAM/Mobile_Development/MyFirstApp/app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.myfirstapp
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +14,11 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val tv = findViewById<TextView>(R.id.textoPrincipal)
+        val btn = findViewById<Button>(R.id.btnSaludar)
+
+        btn.setOnClickListener {
+
+            tv.text = "Bien hecho!"
+        }
     }
 }
